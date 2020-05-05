21ce6b008aa9d6c515dbcee4b1c47a00a559ebef
Abu Buwe <abu@joinzoe.com>
2020-05-05 02:13:29 +0100

    Switch from Selectors to Checkboxes with captions

--- src/features/assessment/TreatmentSelectionScreen.tsx
+++ src/features/assessment/TreatmentSelectionScreen.tsx
@@ -1,22 +1,39 @@
 import { RouteProp } from '@react-navigation/native';
 import { StackNavigationProp } from '@react-navigation/stack';
-import { Form, Text } from 'native-base';
+import { Formik, FormikProps } from 'formik';
+import { Form, Text, CheckBox, Label, View } from 'native-base';
 import React, { Component } from 'react';
 import { StyleSheet } from 'react-native';
+import * as Yup from 'yup';
 
+import { CheckboxItem, CheckboxList } from '../../components/Checkbox';
 import { colors, fontStyles } from '../../../theme';
 import { BigButton } from '../../components/Button';
 import ProgressStatus from '../../components/ProgressStatus';
 import Screen, { FieldWrapper, Header, ProgressBlock } from '../../components/Screen';
-import { CaptionText, HeaderText } from '../../components/Text';
+import { BrandedButton, CaptionText, HeaderText } from '../../components/Text';
 import UserService from '../../core/user/UserService';
 import i18n from '../../locale/i18n';
 import Navigator from '../Navigation';
 import { ScreenParamList } from '../ScreenParamList';
+import { t } from 'i18n-js';
 
 type TreatmentSelectionProps = {
   navigation: StackNavigationProp<ScreenParamList, 'TreatmentSelection'>;
   route: RouteProp<ScreenParamList, 'TreatmentSelection'>;
+};
+
+interface TreatmentData {
+  treatments: string[];
+}
+
+const initalFormValues = {
+  treatments: [] as string[],
+};
+
+type CheckBoxData = {
+  label: string;
+  value: string;
 };
 
 export default class TreatmentSelectionScreen extends Component<TreatmentSelectionProps> {
@@ -26,16 +43,21 @@ export default class TreatmentSelectionScreen extends Component<TreatmentSelecti
     this.handleTreatment = this.handleTreatment.bind(this);
   }
 
-  handleTreatment(treatment: string) {
+  handleTreatment(values: TreatmentData) {
     const { currentPatient, assessmentId, location } = this.props.route.params;
     const userService = new UserService();
 
-    if (treatment == 'other') {
-      this.props.navigation.navigate('TreatmentOther', { currentPatient, assessmentId, location });
-    } else {
-      userService.updateAssessment(assessmentId, { treatment }).then((r) => Navigator.gotoEndAssessment());
-    }
+    // TODO: adjust backend and update below based on question changes.
+    // if (values.treatments.includes('other')) {
+    //   this.props.navigation.navigate('TreatmentOther', { currentPatient, assessmentId, location });
+    // } else {
+    //   userService.updateAssessment(assessmentId, { treatment }).then((r) => Navigator.gotoEndAssessment());
+    // }
   }
+
+  registerSchema = Yup.object().shape({
+    treatments: Yup.array<string>().min(1),
+  });
 
   render() {
     const currentPatient = this.props.route.params.currentPatient;
@@ -84,18 +106,49 @@ export default class TreatmentSelectionScreen extends Component<TreatmentSelecti
           <ProgressStatus step={4} maxSteps={5} />
         </ProgressBlock>
 
-        <Form style={styles.form}>
-          {treatmentSelectionOptions.map((item) => {
+        <Formik
+          initialValues={initalFormValues}
+          validationSchema={this.registerSchema}
+          onSubmit={(values: TreatmentData) => {
+            return this.handleTreatment(values);
+          }}>
+          {(props) => {
             return (
-              <FieldWrapper style={styles.fieldWrapper} key={item.value}>
-                <BigButton onPress={() => this.handleTreatment(item.value)}>
-                  <Text>{item.label}</Text>
-                </BigButton>
-                <CaptionText style={styles.indentedText}>{item.caption}</CaptionText>
-              </FieldWrapper>
+              <View>
+                <FieldWrapper>
+                  <CheckboxList>
+                    {treatmentSelectionOptions.map((item) => {
+                      return (
+                        <CheckboxItem
+                          key={item.value}
+                          value={props.values.treatments.includes(item.value)}
+                          onChange={(checked: boolean) => {
+                            let treatmentArray = props.values.treatments;
+                            if (item.value == 'none') {
+                              treatmentArray = ['none'];
+                            } else if (checked) {
+                              treatmentArray.push(item.value);
+                              treatmentArray = treatmentArray.filter((val) => val !== 'none');
+                            } else {
+                              treatmentArray = treatmentArray.filter((val) => val !== item.value);
+                            }
+                            props.setFieldValue('treatments', treatmentArray);
+                          }}>
+                          {item.label}
+                          {item.caption && <CaptionText>{'\n' + item.caption}</CaptionText>}
+                        </CheckboxItem>
+                      );
+                    })}
+                  </CheckboxList>
+                </FieldWrapper>
+
+                <BrandedButton onPress={props.handleSubmit}>
+                  <Text>{i18n.t('next-question')}</Text>
+                </BrandedButton>
+              </View>
             );
-          })}
-        </Form>
+          }}
+        </Formik>
       </Screen>
     );
   }
