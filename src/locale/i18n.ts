53a66bdafedbcf175f7ec6c8202576be183d4886
Kenneth Read <kenneth@joinzoe.com>
2020-05-14 18:05:44 +0100

    pretty

--- src/locale/i18n.ts
+++ src/locale/i18n.ts
@@ -3,7 +3,7 @@ import i18n from 'i18n-js';
 i18n.fallbacks = true;
 i18n.translations = {
   en: require('../../assets/lang/en.json'),
-  'es': require('../../assets/lang/es.json'),
+  es: require('../../assets/lang/es.json'),
   'en-US': require('../../assets/lang/en-US.json'),
   'sv-SE': require('../../assets/lang/sv-SE.json'),
 };
